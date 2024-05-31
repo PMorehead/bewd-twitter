914425b16b86ea54f9bfd41af73bee78448ae1c4
Patrick Morehead <patrick.a.morehead@gmail.com>
2024-05-31 16:39:39 -0500

    Completed twitter clone

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :username, uniqueness: true, length: { minimum: 3, maximum: 64 }, presence: true
+  validates :email, uniqueness: true, length: { minimum: 5, maximum: 500 }, presence: true
+  validates :password, length: { minimum: 8, maximum: 64 }, presence: true
+
+  has_many :sessions
+  has_many :tweets
 end
